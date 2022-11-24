35e483e6966c1cb965c78b3cfd2654eb1217fe7b
Cndzuzuzu <118964356+Cndzuzuzu@users.noreply.github.com>
2022-11-24 23:33:42 +0700

    Tambah FormStart

---
+++ 160421029_Nico Victorio/160421029_Nico Victorio/FormStart.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace _160421029_Nico_Victorio
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonEmployee = new System.Windows.Forms.Button();
+            this.buttonPengguna = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(199, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "atau";
+            // 
+            // buttonEmployee
+            // 
+            this.buttonEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEmployee.Location = new System.Drawing.Point(164, 254);
+            this.buttonEmployee.Name = "buttonEmployee";
+            this.buttonEmployee.Size = new System.Drawing.Size(97, 30);
+            this.buttonEmployee.TabIndex = 17;
+            this.buttonEmployee.Text = "Employee";
+            this.buttonEmployee.UseVisualStyleBackColor = true;
+            // 
+            // buttonPengguna
+            // 
+            this.buttonPengguna.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPengguna.Location = new System.Drawing.Point(164, 197);
+            this.buttonPengguna.Name = "buttonPengguna";
+            this.buttonPengguna.Size = new System.Drawing.Size(97, 31);
+            this.buttonPengguna.TabIndex = 16;
+            this.buttonPengguna.Text = "Pengguna";
+            this.buttonPengguna.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(168, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Masuk Sebagai:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cinzel Black", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(90, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(271, 18);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "The Application for Digital Banking";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("Cinzel Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Location = new System.Drawing.Point(-7, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(430, 28);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "DiBa";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(161, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Selamat Datang!";
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 339);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonEmployee);
+            this.Controls.Add(this.buttonPengguna);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormStart";
+            this.Text = "FormStart";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonEmployee;
+        private System.Windows.Forms.Button buttonPengguna;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
