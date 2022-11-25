3cb7306a92088505abdc2f854f14166233bbe65d
tygarean <nico.victorio@gmail.com>
2022-11-25 23:01:35 +0700

    apdet form buat akun

--- 160421029_Nico Victorio/160421029_Nico Victorio/FormBuatAkun.Designer.cs
+++ 160421029_Nico Victorio/160421029_Nico Victorio/FormBuatAkun.Designer.cs
@@ -51,52 +51,58 @@ namespace _160421029_Nico_Victorio
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBoxPangkat = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBoxUlangiPin
             // 
             this.textBoxUlangiPin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUlangiPin.Location = new System.Drawing.Point(121, 324);
+            this.textBoxUlangiPin.Location = new System.Drawing.Point(161, 399);
+            this.textBoxUlangiPin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxUlangiPin.Name = "textBoxUlangiPin";
             this.textBoxUlangiPin.PasswordChar = '*';
-            this.textBoxUlangiPin.Size = new System.Drawing.Size(120, 23);
+            this.textBoxUlangiPin.Size = new System.Drawing.Size(159, 26);
             this.textBoxUlangiPin.TabIndex = 41;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(51, 328);
+            this.label10.Location = new System.Drawing.Point(68, 404);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(64, 15);
+            this.label10.Size = new System.Drawing.Size(84, 21);
             this.label10.TabIndex = 40;
             this.label10.Text = "Ulangi PIN";
             // 
             // textBoxPin
             // 
             this.textBoxPin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPin.Location = new System.Drawing.Point(121, 295);
+            this.textBoxPin.Location = new System.Drawing.Point(161, 363);
+            this.textBoxPin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxPin.Name = "textBoxPin";
             this.textBoxPin.PasswordChar = '*';
-            this.textBoxPin.Size = new System.Drawing.Size(120, 23);
+            this.textBoxPin.Size = new System.Drawing.Size(159, 26);
             this.textBoxPin.TabIndex = 39;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(60, 299);
+            this.label9.Location = new System.Drawing.Point(80, 368);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(55, 15);
+            this.label9.Size = new System.Drawing.Size(72, 21);
             this.label9.TabIndex = 38;
             this.label9.Text = "Buat PIN";
             // 
             // buttonBuatAkun
             // 
             this.buttonBuatAkun.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonBuatAkun.Location = new System.Drawing.Point(257, 482);
+            this.buttonBuatAkun.Location = new System.Drawing.Point(343, 593);
+            this.buttonBuatAkun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonBuatAkun.Name = "buttonBuatAkun";
-            this.buttonBuatAkun.Size = new System.Drawing.Size(114, 26);
+            this.buttonBuatAkun.Size = new System.Drawing.Size(152, 32);
             this.buttonBuatAkun.TabIndex = 37;
             this.buttonBuatAkun.Text = "Buat Akun";
             this.buttonBuatAkun.UseVisualStyleBackColor = true;
@@ -105,78 +111,87 @@ namespace _160421029_Nico_Victorio
             // textBoxUlangiPassword
             // 
             this.textBoxUlangiPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUlangiPassword.Location = new System.Drawing.Point(121, 261);
+            this.textBoxUlangiPassword.Location = new System.Drawing.Point(161, 321);
+            this.textBoxUlangiPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxUlangiPassword.Name = "textBoxUlangiPassword";
             this.textBoxUlangiPassword.PasswordChar = '*';
-            this.textBoxUlangiPassword.Size = new System.Drawing.Size(369, 23);
+            this.textBoxUlangiPassword.Size = new System.Drawing.Size(491, 26);
             this.textBoxUlangiPassword.TabIndex = 36;
             // 
             // textBoxPassword
             // 
             this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPassword.Location = new System.Drawing.Point(121, 230);
+            this.textBoxPassword.Location = new System.Drawing.Point(161, 283);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
-            this.textBoxPassword.Size = new System.Drawing.Size(369, 23);
+            this.textBoxPassword.Size = new System.Drawing.Size(491, 26);
             this.textBoxPassword.TabIndex = 35;
             // 
             // textBoxNomorTelepon
             // 
             this.textBoxNomorTelepon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNomorTelepon.Location = new System.Drawing.Point(121, 197);
+            this.textBoxNomorTelepon.Location = new System.Drawing.Point(161, 242);
+            this.textBoxNomorTelepon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxNomorTelepon.Name = "textBoxNomorTelepon";
-            this.textBoxNomorTelepon.Size = new System.Drawing.Size(369, 23);
+            this.textBoxNomorTelepon.Size = new System.Drawing.Size(491, 26);
             this.textBoxNomorTelepon.TabIndex = 34;
             // 
             // textBoxEmail
             // 
             this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxEmail.Location = new System.Drawing.Point(121, 163);
+            this.textBoxEmail.Location = new System.Drawing.Point(161, 201);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxEmail.Name = "textBoxEmail";
-            this.textBoxEmail.Size = new System.Drawing.Size(369, 23);
+            this.textBoxEmail.Size = new System.Drawing.Size(491, 26);
             this.textBoxEmail.TabIndex = 33;
             // 
             // textBoxAlamat
             // 
             this.textBoxAlamat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxAlamat.Location = new System.Drawing.Point(121, 102);
+            this.textBoxAlamat.Location = new System.Drawing.Point(161, 126);
+            this.textBoxAlamat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxAlamat.Multiline = true;
             this.textBoxAlamat.Name = "textBoxAlamat";
             this.textBoxAlamat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxAlamat.Size = new System.Drawing.Size(369, 47);
+            this.textBoxAlamat.Size = new System.Drawing.Size(491, 57);
             this.textBoxAlamat.TabIndex = 32;
             // 
             // textBoxNamaBelakang
             // 
             this.textBoxNamaBelakang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNamaBelakang.Location = new System.Drawing.Point(351, 64);
+            this.textBoxNamaBelakang.Location = new System.Drawing.Point(468, 79);
+            this.textBoxNamaBelakang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxNamaBelakang.Name = "textBoxNamaBelakang";
-            this.textBoxNamaBelakang.Size = new System.Drawing.Size(139, 23);
+            this.textBoxNamaBelakang.Size = new System.Drawing.Size(184, 26);
             this.textBoxNamaBelakang.TabIndex = 31;
             // 
             // textBoxNamaDepan
             // 
             this.textBoxNamaDepan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNamaDepan.Location = new System.Drawing.Point(121, 64);
+            this.textBoxNamaDepan.Location = new System.Drawing.Point(161, 79);
+            this.textBoxNamaDepan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxNamaDepan.Name = "textBoxNamaDepan";
-            this.textBoxNamaDepan.Size = new System.Drawing.Size(120, 23);
+            this.textBoxNamaDepan.Size = new System.Drawing.Size(159, 26);
             this.textBoxNamaDepan.TabIndex = 30;
             // 
             // textBoxNIK
             // 
             this.textBoxNIK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNIK.Location = new System.Drawing.Point(121, 26);
+            this.textBoxNIK.Location = new System.Drawing.Point(161, 32);
+            this.textBoxNIK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBoxNIK.Name = "textBoxNIK";
-            this.textBoxNIK.Size = new System.Drawing.Size(369, 23);
+            this.textBoxNIK.Size = new System.Drawing.Size(491, 26);
             this.textBoxNIK.TabIndex = 29;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(254, 68);
+            this.label8.Location = new System.Drawing.Point(339, 84);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(91, 15);
+            this.label8.Size = new System.Drawing.Size(119, 21);
             this.label8.TabIndex = 28;
             this.label8.Text = "Nama Belakang";
             // 
@@ -184,9 +199,10 @@ namespace _160421029_Nico_Victorio
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(20, 265);
+            this.label7.Location = new System.Drawing.Point(27, 326);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(95, 15);
+            this.label7.Size = new System.Drawing.Size(126, 21);
             this.label7.TabIndex = 27;
             this.label7.Text = "Ulangi Password";
             // 
@@ -194,9 +210,10 @@ namespace _160421029_Nico_Victorio
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(58, 234);
+            this.label6.Location = new System.Drawing.Point(77, 288);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(57, 15);
+            this.label6.Size = new System.Drawing.Size(78, 21);
             this.label6.TabIndex = 26;
             this.label6.Text = "Password";
             // 
@@ -204,9 +221,10 @@ namespace _160421029_Nico_Victorio
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(26, 201);
+            this.label5.Location = new System.Drawing.Point(35, 247);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(89, 15);
+            this.label5.Size = new System.Drawing.Size(117, 21);
             this.label5.TabIndex = 25;
             this.label5.Text = "Nomor Telepon";
             // 
@@ -214,9 +232,10 @@ namespace _160421029_Nico_Victorio
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(78, 167);
+            this.label4.Location = new System.Drawing.Point(104, 206);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.Size = new System.Drawing.Size(49, 21);
             this.label4.TabIndex = 24;
             this.label4.Text = "Email";
             // 
@@ -224,9 +243,10 @@ namespace _160421029_Nico_Victorio
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(69, 106);
+            this.label3.Location = new System.Drawing.Point(92, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.Size = new System.Drawing.Size(61, 21);
             this.label3.TabIndex = 23;
             this.label3.Text = "Alamat";
             // 
@@ -234,9 +254,10 @@ namespace _160421029_Nico_Victorio
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(38, 68);
+            this.label2.Location = new System.Drawing.Point(51, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.Size = new System.Drawing.Size(100, 21);
             this.label2.TabIndex = 22;
             this.label2.Text = "Nama Depan";
             // 
@@ -244,25 +265,39 @@ namespace _160421029_Nico_Victorio
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(88, 30);
+            this.label1.Location = new System.Drawing.Point(117, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.Size = new System.Drawing.Size(36, 21);
             this.label1.TabIndex = 21;
             this.label1.Text = "NIK";
             // 
             // comboBoxPangkat
             // 
             this.comboBoxPangkat.FormattingEnabled = true;
-            this.comboBoxPangkat.Location = new System.Drawing.Point(121, 364);
+            this.comboBoxPangkat.Location = new System.Drawing.Point(161, 448);
+            this.comboBoxPangkat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxPangkat.Name = "comboBoxPangkat";
-            this.comboBoxPangkat.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPangkat.Size = new System.Drawing.Size(160, 24);
             this.comboBoxPangkat.TabIndex = 42;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(83, 448);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 21);
+            this.label11.TabIndex = 43;
+            this.label11.Text = "Pangkat";
             // 
             // FormBuatAkun
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 520);
+            this.ClientSize = new System.Drawing.Size(804, 640);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.comboBoxPangkat);
             this.Controls.Add(this.textBoxUlangiPin);
             this.Controls.Add(this.label10);
@@ -285,6 +320,7 @@ namespace _160421029_Nico_Victorio
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormBuatAkun";
             this.Text = "FormBuatAkun";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormBuatAkun_FormClosing);
@@ -318,5 +354,6 @@ namespace _160421029_Nico_Victorio
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBoxPangkat;
+        private System.Windows.Forms.Label label11;
     }
 }
